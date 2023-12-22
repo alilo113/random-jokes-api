5773bc3c835ac0f11dcdfb9c8e00d09a54fcf3fb
unknown <aliopdandan@gmail.com>
2023-12-22 12:34:37 +0100

    commit

--- api.py
+++ api.py
@@ -3,23 +3,26 @@ import requests
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 
-jokes = []
 url = "https://www.cultureamp.com/blog/funny-jokes-for-the-workplace"
 response = requests.get(url)
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    quotes = soup.find_all('span', class_='text')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    jokes = soup.find_all('li')  # Assuming jokes are within <p> tags
 
-    for quote in quotes:
-        jokes.append(quote.get_text())
+    joke_texts = []
+    for joke in jokes:
+        text = joke.get_text()
+        if text.strip():  # Check if the text content is not empty
+            joke_texts.append(text)
+
 
 app = Flask(__name__)
 
 @app.route("/random")
 def first_app():
-    random_joke = random.choice(jokes)
+    random_joke = random.choice(joke_texts)  # Use joke_texts instead of jokes
     return jsonify({"joke": random_joke})
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
